1d6e38d90b851cc929b25692a14b885e37a5408c
Pablo Gutierrez <pablo@vivalacloud.com>
2014-07-10 08:54:58 +0200

    0.1

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Prisma theme
 ===========
 
 The Prisma theme includes: backgrounds, icons, gtk theme &amp; gnome-shell theme.
+
+Current version 0.1
---
+++ install.sh
@@ -0,0 +1,11 @@
+##########################################################
+#
+# Author: Pablo Gutierrez Alfaro <pablo@prismaos.com>
+# Last edit: 10-07-2014
+# Version: 0.1
+# Description: Bash to install all the Prisma Theme
+# WARNING! This code will delete all the other themes
+# in your system
+#
+##########################################################
+#!/bin/bash
